d8d0e34f4b8a3db6d2d6657a793c46d2231dbdf1
Khan B <itisameerkhan@gmail.com>
2025-05-02 15:24:36 +0530

    loading config

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import "./index.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "./components/Header/Header";
 import { useDispatch } from "react-redux";
 import { addUser } from "./utils/userSlice";
+import Loader from "./components/Loader/Loader";
 
 const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
 
   const authenticateFunction = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.get(
         `${import.meta.env.VITE_BASE_URL}/user/authenticate`,
         { withCredentials: true }
@@ -20,6 +23,7 @@ const App = () => {
 
       if (!response.data.success) {
         navigate("/");
+        setIsLoading(false);
       } else {
         const response = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/user/get/info`,
@@ -34,10 +38,15 @@ const App = () => {
           navigate(`${location.pathname}`);
         }
       }
+      setIsLoading(false);
     } catch (e) {
+      setIsLoading(false);
+
       console.log(e);
     }
   };
+
+  if(isLoading) return <Loader />;
 
   useEffect(() => {
     authenticateFunction();
