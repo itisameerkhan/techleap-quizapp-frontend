d471f2dfc71abb1eda21f45b99c4db7f547f5925
Khan B <itisameerkhan@gmail.com>
2025-05-02 15:37:06 +0530

    loading config

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import axios from "axios";
 import Header from "./components/Header/Header";
 import { useDispatch } from "react-redux";
 import { addUser } from "./utils/userSlice";
-import Loader from "./components/Loader/Loader";
 
 const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
 
   const authenticateFunction = async () => {
     try {
-      setIsLoading(true);
       const response = await axios.get(
         `${import.meta.env.VITE_BASE_URL}/user/authenticate`,
         { withCredentials: true }
@@ -23,7 +20,6 @@ const App = () => {
 
       if (!response.data.success) {
         navigate("/");
-        setIsLoading(false);
       } else {
         const response = await axios.get(
           `${import.meta.env.VITE_BASE_URL}/user/get/info`,
@@ -38,15 +34,12 @@ const App = () => {
           navigate(`${location.pathname}`);
         }
       }
-      setIsLoading(false);
     } catch (e) {
-      setIsLoading(false);
-
       console.log(e);
     }
   };
 
-  if(isLoading) return <Loader />;
+
 
   useEffect(() => {
     authenticateFunction();
